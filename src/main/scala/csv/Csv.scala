5696df9d01bb10ed9fe8156fc229e3586f63fed7
Harimurti Prasetio <harippe@protonmail.com>
2019-08-14 12:28:08 +0700

    update csv parsing

---
+++ src/main/scala/csv/Csv.scala
@@ -0,0 +1,28 @@
+package csv
+
+case class Csv(rows: List[Row]) {
+  override def toString: String = {
+    val str = rows.map(_.toString).mkString("\n")
+    str
+  }
+}
+case class Row
+(
+  temperature: Double,
+  temperatureMin: Double,
+  temperatureMax: Double,
+  pressure: Double,
+  seaLevel: Double,
+  groundLevel: Double,
+  humidity: Int,
+  weatherDesc: String,
+  windSpeed: Double,
+  windDegree: Double,
+  dateText: String,
+  cityName: String
+) {
+  override def toString = List(
+    temperature,
+    temperatureMin
+  ).mkString(",")
+}
