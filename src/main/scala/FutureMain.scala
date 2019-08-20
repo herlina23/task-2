0e0f7549264d59b492dc7f736a0cdaef76f628ad
herlina23 <frois.herlina@gmail.com>
2019-08-20 09:50:50 +0700

    new explanation

--- src/main/scala/FutureMain.scala
+++ src/main/scala/FutureMain.scala
@@ -22,99 +22,162 @@ object Main {
 
   def main(args: Array[String]): Unit = {
     val hdr = List("Temperature","Temperature Min","Temperature Max","Pressure","Sea Level","Ground Level","Humidity","Description","Wind Speed","Wind Degree","Date","City")
-    // val hdr adalah list yang berisi nama-nama header yang akan ditampilkan pada file CSV
+        /**
+      * hdr adalah variabel dengan tipe List[A]
+      * berisi list header yang akan digunakana pada CSV
+      * */
 
     val future: Future[Unit] = Future {
-      // val future mendefinisikan suatu variabel yang berbentuk unit dengan menggunakan metode Future sebagai jenis metode asyncronous
+            /**
+        * future adalah variabel dengan tipe Future[Unit]
+        * didalam Future{} terdapat aktivitas yang akan dieksekusi secara asyncronous
+        * */
 
+      /**
+        *
+        * */
       val url = "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=f8c2705abbe43a56eaca373e8d86070e"
-      // url mendefinisikan link API
+      // url adalah variabel mendefinisikan link API
 
       val jsonStr = io.Source.fromURL(url).getLines.mkString
-      // jsonStr berfungsi mengambil data dari API yang sudah didefinisi, dimana data dari API dibaca dengan  io.Source.fromURL,dibaca per line dengan .getLines dan melakukan join tiap line dengan .mkString
-      // @correction = jsonStr adalah variabel, tidak mungkin ada aksi aktif yang bisa dikerjakan oleh variabel
-      val jsonValue = Json.parse(jsonStr)
-      // jsonValue melakukan parsing dari suatu string JSON menjadi suatu scala object
-      // @correction = jsonValue adalah variabel, tidak mungkin ada aksi aktif yang bisa dikerjakan oleh variabel
-      val forecastOpt = jsonValue.asOpt[Forecast]
-      //forecastOpt melakukan konversi dari suatu json value menjadi suatu collection Forecast dengan menggunakan asOpt menjadi suatu Option type, sehingga program akann tetap berjalan meskipun ada kesalahan
-      // @correction = forecastOpt adalah variabel, tidak mungkin ada aksi aktif yang bisa dikerjakan oleh variabel
       /**
-        * @correction
-        * forecastOpt adalah variabel dengan tipe Option[Forecast]
-        * ketika forecastOpt.map() dijalankan, map() akan mengubah concrete value yang ada di dalam forecastOpt
-        * berdasarkan anonymous function yang jadi argumen map() jika forecastOpt adalah Some[Forecast].
-        * Jika forecastOpt adalah None, map() tidak akan bisa mengubah None menjadi None lain
+        * jsonStr adalah variabel dimana ketika dijalankan, io.Source.fromURL akan membaca data dari link variabel url menjadi BufferedSource
+        * kemudian getLines akan mengubah buffered source menjadi non empty iterator
+        * setelah itu, .mkString akan menjadikan non-empty-iterator menjadi string
         * */
+
+      val jsonValue = Json.parse(jsonStr)
+      /**
+        * jsonValue adalah variabel yang ketika dijalankan, Json.parse() akan menjadikan jsonStr yang merupakan JSON String menjadi jsonvalue
+        * */
+
+      val forecastOpt = jsonValue.asOpt[Forecast]
+      /**
+        * forecastOpt adalah variabel yang bertipe Option[Forecast]
+        * */
+
       val csvOpt = forecastOpt.map { forecast =>
-
-        //pada csvOpt kita akan melakukan mapping pada collection forecastOpt, collection yang akan di-mapping didalamnya adalah forecast.
-
         /**
           * @correction
-          * forecast adalah concrete value bertipe Forecast yang dikandung oleh forecastOpt jika dan hanya jika forecastOpt bertipe Some[Forecast]
-          * forecast.weatherList adalah field dari Forecast yang bertipe data List[WeatherList]
-          * oleh karena itu ketika forecast.weatherList.map() dijalankan, map() akan mengubah List[WeatherList] menjadi List[A],
-          * sesuai dengan tipe kembalian dari anonymous function yang menjadi argumen dari map
-          * untuk kasus di bawah, map() memiliki tipe WeatherList => Row, sehingga map() di bawah ini
-          * akan mengubah List[WeatherList] menjadi List[Row]
-          *
+          * forecastOpt adalah variabel dengan tipe Option[Forecast]
+          * ketika forecastOpt.map() dijalankan, map() akan mengubah concrete value yang ada di dalam forecastOpt
+          * berdasarkan anonymous function yang jadi argumen map() jika forecastOpt adalah Some[Forecast].
+          * Jika forecastOpt adalah None, map() tidak akan bisa mengubah None menjadi None lain
           * */
+
         val rowList = forecast.weatherList.map { weatherList =>
-          //setelah memasuki collection forecast, kita akan melaukkan mapping, untuk mendapat value dari weatherlist yang berada di dalam collection forecast
-          // @correction = forecast bukan collection
+          /**
+            * @correction
+            * forecast adalah concrete value bertipe Forecast yang dikandung oleh forecastOpt jika dan hanya jika forecastOpt bertipe Some[Forecast]
+            * forecast.weatherList adalah field dari Forecast yang bertipe data List[WeatherList]
+            * oleh karena itu ketika forecast.weatherList.map() dijalankan, map() akan mengubah List[WeatherList] menjadi List[A],
+            * sesuai dengan tipe kembalian dari anonymous function yang menjadi argumen dari map
+            * untuk kasus di bawah, map() memiliki tipe WeatherList => Row, sehingga map() di bawah ini
+            * akan mengubah List[WeatherList] menjadi List[Row]
+            *
+            * */
+
           Row(
-            // Row -> mengakses sebuat case class didalam Csv yang berisi definisi dari kolom-kolom yang akan ditampilkan pada file CSV
+            /**
+              * Row adalah sebuah case class yang berisi beberapa field yang akan di-write ke CSV
+              * */
 
             weatherList.main1.temperature, weatherList.main1.temperatureMin, weatherList.main1.temperatureMax, weatherList.main1.pressure, weatherList.main1.seaLevel, weatherList.main1.groundLevel, weatherList.main1.humidity, weatherList.weather.map(_.description).mkString, weatherList.wind.speed, weatherList.wind.degree, weatherList.dateText, forecast.city.name
             //diatas ini adalah isi dari tiap field yang akan ditampilkan pada CSV
+            /**
+              * didalam Row() terdapat beberapa parameter
+              * parameter tersebut diakses dari  weatherList.main1 bertipe List[main1],
+              * dimana didalamnya terdapat beberapa value seperti temperature, pressure dll
+              * */
+
           )
         }
         Csv(rowList.toList, Header(hdr))
-        // csv adalah suatu class yang memiliki parameter berupa body dari csv ->rowList.toList(dari collection Row dikonversi menjadi List) dan Header untuk file pada csv-nya->Header(hd) berupa list nama eader
+        // csv adalah suatu class yang memiliki parameter berupa body dari csv ->rowList.toList(dari collection Row dikonversi menjadi List) dan Header untuk file pada csv-nya->Header(hd) berupa list nama header
+        /**
+          * Csv adalah case class yang memiliki 2 parameter input, yaitu body dan header
+          * rowList adalah variable bertipe seq[Row] hasil dari mapping dari forecast.weatherList.map
+          * kemudian untuk menjadikannya list makan rowList dikonversi .toList
+          * Header() adalah case class yang berisi hdr, yang merupakan List[] dari header yang akan di-write ke CSV
+          * * */
 
       }
       csvOpt
-      // mendefinisikan variabel csvOpt
-      // @correction = csvOpt adalah sebuah variabel bertipe Option[Csv]
-      //dibawah ini adalah method untuk melakukan validasi (success or failure)
+      /**
+        *csvOpt adalah sebuah variabel bertipe Option[Csv]
+        * */
+
     }.flatMap { csvOpt =>
+      /**
+        * flatmap() akan mengubah Option[Csv] menjadi Future[S]
+        * */
       //setelah didefinisikan (namanya apa ?) sbg collection, maka kita akan melakuka flatmap()
       // menggabungkan banyak array (nested) menjadi satu kesatuan Array. yang nantinya memudahkan untuk melakukan validasi
       // @correction = penjelasan di atas tidak jelas, kemudian flatMap di kasus ini tidak melibatkan Array
+
       csvOpt match {
         // disini kita akan melakukan match terhadap suatu kondisi yang akan dihadapi oleh collection csvOpt
+        /**
+          *match {} mengubah match statement menjadi pattern matching
+          * */
 
         case Some(cc) => Future {
-          // case  pertama adalah some, berisi aktivitas yang dijalankan bila program berhasil dijalankan
-          // penggunaan some, karena diatas kita mengakses suatu nilai dari container Option, maka matching pattern-nya menggunakan some untuk success  result
+          /**
+            * case Some(A), berisi aktivitas yang dijalankan bila program berhasil dieksekusi
+            * Future {} berisi multiple implementation
+            * */
 
           val cc = csvOpt.toList.mkString("\n")
-          //cc adalah collection csvOpt yang sudah dikonversi menjadi list yang sudah dipisahkan dengan new line("\n")
+          //cc adalah variabel yang didalmnya terdapat csvOpt yang bertipe Option[Csv]
+          // yang sudah dikonversi menjadi list yang sudah dipisahkan dengan new line("\n")
+          /**
+            * cc adalah variabel yang didalmnya terdapat csvOpt yang bertipe Option[Csv]
+            * dimana csvOpt akan dijadikan List dengan .toList
+            * setelah itu tiap nilai dari List akan diprint dengan new line("\n") dengan .mkString
+            * */
 
           val pw = new PrintWriter(new File("weather_future.csv" ))
           // pw menghasilkan new class dari PrintWriter untuk melakukan write pada file system, dengan path dan nama tertentu
+          /**
+            *variabel pw mengakses class yang disediakan oleh java io
+            * pertama variabel pw akan mengakses new class PrintWriter(File file), dimana parameternya adalah File
+            * kedua, new PrintWriter mengakses new class File(String Path Name), dimana parameternya adalah path dimana file akan disimpan
+            * */
 
           pw.write(cc)
           // setelah itu menuliskan value yang disimpan pada variabel cc ke file csv
+          /**
+            *write(cc) akan menuliskan file yang disimpan dalam variabel cc ke dalam file csv
+            * */
 
           pw.close
-          //penulisan ditutup
+          /**
+            *close akan menutup stream penulisan ke file csv
+            * */
 
           println("Csv is already generated")
-          // bila file csv sudah di-generate maka akan menampilkan pesan di atas
+          /**
+            *bila file csv sudah di-generate maka akan menampilkan pesan di atas
+            * */
         }
         case None => println("something bad happened; maybe jsonStr is not a valid json string")
-          // case kedua adlah none, termasuk matching pattern dari Option, yang akan menampilkan pesan error
+                   /**
+            *None bertipe Option[Nothing], berisi pesan exception
+            * */
           Future.failed(new RuntimeException())
-        // apabila failed, maka Future akan melakukan throw exception
+        /**
+          *apabila failed, maka Future akan melakukan throw exception
+          * */
       }
     }
 
 
     val result: Unit = Await.result(future,1.minute)
     // val result digunakan untuk memanggil hasil dari eksekusi Future dimana pemanggilan hasil Future dengan Await, dengan 2 parameter input, yaitu variabel future dan durasi
-
+    /**
+      * val result bertipe Unit yang akan menampung hasil balikan Future dengan fungsi Await
+      * .result(Awaitable[T],Duration) memiliki 2 parameter yaitu Awaitable yang berupa variabel yang telah mengimplementasi Future dan Duration
+      * */
 
 
 
